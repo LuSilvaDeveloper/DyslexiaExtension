a152caf0696d7d4a48d9d8a7769cb4f96dd9648e
DESKTOP-RJMDTP3\owner <Jonathanliu865@gmail.com>
2023-09-23 13:53:59 -0700

    Completed popup for extension including the switches

--- DyslexiaExtensionChrome/src/popup/Popup.jsx
+++ DyslexiaExtensionChrome/src/popup/Popup.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './Popup.css'
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import Form from "react-bootstrap/Form";
+import { Container } from "react-bootstrap";
 
 function App() {
   const [crx, setCrx] = useState('create-chrome-ext')
@@ -11,8 +13,20 @@ function App() {
       <h3>Popup Page!</h3>
 
       <h6>v 0.0.0</h6>
-
-      <Button variant="dark">hehehe</Button>
+      <Container className="mt-5">
+      <Form>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label="Customize Font"
+        />
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label="Stop Animations"
+        />
+      </Form>
+    </Container>
     </main>
   )
 }
